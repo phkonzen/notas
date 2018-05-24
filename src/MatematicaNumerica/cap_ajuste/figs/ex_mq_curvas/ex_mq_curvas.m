2e569ec9d47893b9a98f75f7186bca761720f9f7
Pedro Konzen <phkonzen@gmail.com>
2018-05-24 18:22:49 -0300

    + sobre problemas lineares de ajuste por mínimos quadrados

---
+++ src/MatematicaNumerica/cap_ajuste/figs/ex_mq_curvas/ex_mq_curvas.m
@@ -0,0 +1,15 @@
+#pontos
+x = [-1 0 1 1.5]';
+y = [1.2, -0.1, 0.7, 2.4]';
+
+#resol. as eqs. normais
+A = [sin(x) cos(x) ones(4,1)];
+c = inv(A'*A)*A'*y
+
+#curva ajustada
+f = @(x) c(1)*sin(x) + c(2)*cos(x) + c(3)
+
+#esboco do pol. ajustado
+xx = linspace(-1.25,1.75);
+plot(x,y,'ro',...
+     xx,f(xx));grid
