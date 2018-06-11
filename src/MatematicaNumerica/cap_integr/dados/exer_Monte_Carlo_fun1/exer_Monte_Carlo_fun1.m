3c5d480659d1cfd06c3a63311b9fc86c910178e4
Pedro Konzen <phkonzen@gmail.com>
2018-06-11 17:13:20 -0300

    +:material sobre o método de Monte Carlo

---
+++ src/MatematicaNumerica/cap_integr/dados/exer_Monte_Carlo_fun1/exer_Monte_Carlo_fun1.m
@@ -0,0 +1,14 @@
+rand("seed",0)
+f = @(x) (sin(x+2)-exp(-x^2))/sqrt(1-x^ 2);
+a=0;
+b=1;
+n=100000;
+s=0;
+for i=1:n
+  x=a + (b-a)*rand();
+  s+=f(x);
+endfor
+s*=(b-a)/n;
+
+ia=0.5-exp(-1)/2;
+printf("%1.5E %1.1E\n",s,abs(ia-s))
