d0c8dac7e043c4bc6eac4a5a6943c35e7f10f421
Pedro Konzen <phkonzen@gmail.com>
2018-06-12 07:30:43 -0300

    +:exercício sobre o método de Monte Carlo;*:correções ortográficas

--- src/MatematicaNumerica/cap_integr/dados/exer_Monte_Carlo_fun1/exer_Monte_Carlo_fun1.m
+++ src/MatematicaNumerica/cap_integr/dados/exer_Monte_Carlo_fun1/exer_Monte_Carlo_fun1.m
@@ -1,14 +1,32 @@
-rand("seed",0)
-f = @(x) (sin(x+2)-exp(-x^2))/sqrt(1-x^ 2);
-a=0;
+rand("seed",time())
+f = @(x) (sin(x+2)-exp(-x^2))/(x^2+log(x+2));
+a=-1;
 b=1;
-n=100000;
-s=0;
+
+#num. max. iter.
+n=999999;
+#TOL
+TOL=1e-3;
+#cont. conv.
+c=0;
+
+#iteracoes
+s0=0;
+s1=0;
 for i=1:n
   x=a + (b-a)*rand();
-  s+=f(x);
+  s1*=(i-1);
+  s1+=(b-a)*f(x);
+  s1/=i;
+  if (mod(i,10000)==0)
+    if (abs(s1-s0)<TOL)
+      c+=1;
+    else
+      s0=s1;
+    endif
+    if (c==2)
+      break;
+    endif
+  endif
 endfor
-s*=(b-a)/n;
-
-ia=0.5-exp(-1)/2;
-printf("%1.5E %1.1E\n",s,abs(ia-s))
+printf("%d %1.5E\n",i,s1)
