1752d23a76cc28b9eca7871b7e6eb9f4977349e5
Pedro Konzen <phkonzen@gmail.com>
2019-08-05 20:22:40 -0300

    *: atualização do material sobre funções afins

--- src/CalculoI/cap_funcao/dados/fig_ex_funlinear/fig_ex_funlinear.py
+++ src/CalculoI/cap_funcao/dados/fig_ex_funlinear/fig_ex_funlinear.py
@@ -1,15 +1,29 @@
 from sympy import *
 var('x')
 
-p=plot(-5/2,(x,-2,2),line_color='blue',show=False)
-q=plot(2,(x,-2,2),line_color='red',show=False)
+p=plot(0.5*x,(x,-2,2),line_color='blue',show=False)
+q=plot(x,(x,-2,2),line_color='red',show=False)
 p.extend(q)
-q=plot(2*x-1,(x,-2,2),line_color='green',show=False)
+q=plot(2*x,(x,-2,2),line_color='green',show=False)
 p.extend(q)
-p[0].label="$y=-5/2$"
-p[1].label="$y=2$"
-p[2].label="$y=2x-1$"
-p.legend=True
+q=plot(-2*x,(x,-2,2),line_color='green',show=False)
+p.extend(q)
+q=plot(-x,(x,-2,2),line_color='red',show=False)
+p.extend(q)
+q=plot(-0.5*x,(x,-2,2),line_color='blue',show=False)
+p.extend(q)
 p.xlabel="$x$"
 p.ylabel="$y$"
 p.save('fig_ex_funlinear.png')
+
+fig = p._backend.fig
+ax = fig.axes[0]
+ax.grid()
+ax.text(1.5,0.45,'$y=\\frac{1}{2}x$',color='blue')
+ax.text(1.5,1.1,'$y=x$',color='red')
+ax.text(1.5,2.8,'$y=2x$',color='green')
+ax.text(-1.5,3.1,'$y=-2x$',color='green')
+ax.text(-1.5,1.7,'$y=-x$',color='red')
+ax.text(-1.5,0.7,'$y=-\\frac{1}{2}x$',color='blue')
+
+fig.savefig('fig_ex_funlinear.png')
