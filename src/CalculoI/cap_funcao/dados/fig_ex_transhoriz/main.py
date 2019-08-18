9875d6f52017bfe256192d9b948fbce1f52ac525
Pedro Konzen <phkonzen@gmail.com>
2019-08-18 12:59:53 -0300

    +: material sobre operações com funções; *: material sobre logaritmo

---
+++ src/CalculoI/cap_funcao/dados/fig_ex_transhoriz/main.py
@@ -0,0 +1,22 @@
+from sympy import *
+init_printing()
+var('x')
+
+k = 1
+f = lambda x: x**2
+
+p = plot(f(x),(x,-3,3),line_color="gray",show=False)
+q = plot(f(x+k),(x,-3,3),line_color="blue",show=False)
+p.extend(q)
+p.title = ("$k = %1.1f$" % k)
+p.xlabel = '$x$'
+p.ylabel = '$y$'
+p[0].label = "$f(x) = x^2$"
+p[1].label = "$f(x+k)$"
+p.save('fig_ex_transhoriz.png')
+
+fig = p._backend.fig
+ax = fig.axes[0]
+ax.grid()
+ax.legend(loc="upper right")
+fig.savefig('fig_ex_transhoriz.png', bbox_inches='tight')
