22b6639d89046d42ba3805598aa1ad71f7bea93a
Pedro Konzen <phkonzen@gmail.com>
2019-10-14 17:05:00 -0300

    *: correções e ajustes

--- src/CalculoI/cap_apderiv/dados/fig_ex_fcont/main.py
+++ src/CalculoI/cap_apderiv/dados/fig_ex_fcont/main.py
@@ -7,16 +7,19 @@ var('x',real=True)
 
 # a) f(x) = (x-1)^2+1
 p = plot((x-1)**2+1,(x,0,1.5),show=False)
-q = plot(-1,(x,-1,2),line_color='none',show=False)
+q = plot(-0.5,(x,-0.5,2),line_color='none',show=False)
+p.extend(q)
+q = plot(2.5,(x,-0.5,2),line_color='none',show=False)
 p.extend(q)
 p.xlabel = '$x$'
-p.ylabel = '$f(x) = x^2$'
+p.ylabel = '$y$'
 p.save('fig_f.png')
 
 fig = p._backend.fig
 ax = fig.axes[0]
-ax.plot([1],[1],marker='o',color='blue')
-ax.plot([0],[2],marker='o',color='blue')
+ax.plot([1],[1],marker='o',color='red')
+ax.plot([0],[2],marker='o',color='red')
+ax.plot([1.5],[(1.5-1)**2+1],marker='o',color='blue',zorder=10)
 ax.plot([1.5,1.5],[0,0.5**2+1],ls='--',color='gray')
 fig.savefig('fig_f.png')
 
@@ -25,12 +28,14 @@ p = plot(log(x),(x,0.01,E),ylim=[-4,2],show=False)
 q = plot(2,(x,-1,3),line_color='none',show=False)
 p.extend(q)
 p.xlabel = '$x$'
-p.ylabel = '$g(x) = \\ln\,x$'
+p.ylabel = '$y$'
 p.save('fig_g.png')
 
 fig = p._backend.fig
 ax = fig.axes[0]
-ax.plot([E],[1],marker='o',color='blue')
+ax.set_xticks([-1,0,1,2.7182])
+ax.set_xticklabels(["$-1$","$0$","$1$","$e$"])
+ax.plot([E],[1],marker='o',color='red',zorder=12)
 ax.plot([E,E],[0,1],ls='--',color='gray')
 ax.plot([0,E],[1,1],ls='--',color='gray')
 fig.savefig('fig_g.png')
@@ -38,16 +43,21 @@ fig.savefig('fig_g.png')
 # c) h(x) = x, 0<=x<1; h(x) = 0, x=1.
 p = plot(x,(x,0,1),ylim=[-0.5,1.5],show=False)
 q = plot(-0.5,(x,-0.5,1.5),line_color='none',show=False)
+p.extend(q)
 q = plot(1.5,(x,-0.5,1.5),line_color='none',show=False)
 p.extend(q)
 p.xlabel = '$x$'
-p.ylabel = '$h(x)$'
+p.ylabel = '$y$'
 p.save('fig_h.png')
 
 fig = p._backend.fig
 ax = fig.axes[0]
-ax.plot([0],[0],marker='o',color='blue')
-ax.plot([1],[1],marker='o',color='blue',markerfacecolor='none')
+ax.set_xticks([0,1])
+ax.set_xticklabels(["$0$","$1$"])
+ax.set_yticks([0,1])
+ax.set_yticklabels(["$0$","$1$"])
+ax.plot([0],[0],marker='o',color='red')
+ax.plot([1],[1],marker='o',color='blue',markerfacecolor='white',zorder=10)
 ax.plot([1],[0],marker='o',color='blue')
 ax.plot([1,1],[0,1],ls='--',color='gray')
 ax.plot([0,1],[1,1],ls='--',color='gray')
--- src/CalculoI/cap_apderiv/dados/fig_ex_vmaxminabs/main.py
+++ src/CalculoI/cap_apderiv/dados/fig_ex_vmaxminabs/main.py
@@ -10,12 +10,15 @@ p = plot(x**2,(x,-2,2),ylim=[-1,4],show=False)
 q = plot(-1,(x,-2,2),line_color='none',show=False)
 p.extend(q)
 p.xlabel = '$x$'
-p.ylabel = '$f(x) = x^2$'
+p.ylabel = '$y$'
 p.save('fig_f.png')
 
 fig = p._backend.fig
 ax = fig.axes[0]
-ax.plot([0],[0],marker='o',color='blue')
+ax.set_xticks([0])
+ax.set_yticks([])
+ax.plot([0],[0],marker='o',color='red')
+ax.text(1,4,"$f(x) = x^2$",fontsize=16)
 fig.savefig('fig_f.png')
 
 # g(x) = -x^2
@@ -23,19 +26,29 @@ p = plot(-x**2,(x,-2,2),ylim=[-4,1],show=False)
 q = plot(1,(x,-2,2),line_color='none',show=False)
 p.extend(q)
 p.xlabel = '$x$'
-p.ylabel = '$g(x) = -x^2$'
+p.ylabel = '$y$'
 p.save('fig_g.png')
 
 fig = p._backend.fig
 ax = fig.axes[0]
-ax.plot([0],[0],marker='o',color='blue')
+ax.set_xticks([0])
+ax.set_yticks([])
+ax.plot([0],[0],marker='o',color='red')
+ax.text(0.5,-4,"$g(x) = -x^2$",fontsize=16)
 fig.savefig('fig_g.png')
 
 # h(x) = x^3
 p = plot(x**3,(x,-2,2),ylim=[-4,4],show=False)
 p.xlabel = '$x$'
-p.ylabel = '$h(x) = x^3$'
+p.ylabel = '$y$'
 p.save('fig_h.png')
+
+fig = p._backend.fig
+ax = fig.axes[0]
+ax.set_xticks([0])
+ax.set_yticks([])
+ax.text(0.5,4,"$h(x) = x^3$",fontsize=16)
+fig.savefig('fig_h.png')
 
 
 
