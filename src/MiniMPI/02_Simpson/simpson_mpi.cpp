8eb704281cf786d77f051c0ca5be8d3ffa49a496
Pedro Konzen <phkonzen@gmail.com>
2020-01-08 14:13:02 -0300

    *: pequenos ajustes

--- src/MiniMPI/02_Simpson/simpson_mpi.cpp
+++ src/MiniMPI/02_Simpson/simpson_mpi.cpp
@@ -19,41 +19,55 @@ int main() {
   int nproc;
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
+  // aborta caso nproc != 2
   assert(nproc == 2);
   
   // identificação (rank) do processo
   int pid;
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-  
+  // limites de integração
   double a = 0;
   double b = 9999999;
+
+  // num. de subintervalos na quadratura
   int n = 999999998;
 
   double h = (b-a)/n;
 
-  double s1 = 0;
+  // computa s0
+  double s0 = 0;
   if (pid == 0) {
+    
     for (int j=1; j<n/2; j++)
-      s1 += fun(a+2*j*h);
+      s0 += fun(a+2*j*h);
 
-    MPI_Send(&s1, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+    // envia s0 para pid=1
+    MPI_Send(&s0, 1, MPI_DOUBLE, 1,
+	     0, MPI_COMM_WORLD);
   }
 
-  double s2 = 0;
+  // computa s1
+  double s1 = 0;
   if (pid == 1) {
+    
     for (int j=1; j<=n/2; j++)
-      s2 += fun(a+(2*j-1)*h);
+      s1 += fun(a+(2*j-1)*h);
 
-    MPI_Recv(&s1, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // recebe s0 do pid=1
+    MPI_Recv(&s1, 1, MPI_DOUBLE, 0,
+	     0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-      std::cout << std::setprecision(10)
-		<< h/3*(fun(a)+2*s1+4*s2+fun(b))
-		<< std::endl
-		<< b*sin(b)+cos(b)-(a*sin(a)+cos(a))
-		<< std::endl;
+    // imprime a solução
+    std::cout << "simpson = "
+	      << h/3*(fun(a)+2*s0+4*s1+fun(b))
+	      << std::endl
+	      << "analítica = "
+	      << b*sin(b)+cos(b)-(a*sin(a)+cos(a))
+	      << std::endl;
   }
 
+  // finaliza MPI
   MPI_Finalize();
 
   return 0;
