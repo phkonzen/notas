ae5456662aa88c848b91692b0c605940885f09de
Pedro H A Konzen <phkonzen@gmail.com>
2021-03-30 15:11:46 -0300

    +: envio e recebimento de dados

---
+++ src/MatematicaNumericaParalela/cap_mpi/dados/cc_IsendRecv/isendRecv.cc
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// API MPI
+#include <mpi.h>
+
+int main (int argc, char** argv) {
+  // Inicializa o MPI
+  MPI_Init(NULL, NULL);
+
+  // número total de processos
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  if (world_size < 2) {
+    printf ("Num. de processos deve"\
+	    "maior que 2.\n");
+    int errorcode = -1;
+    MPI_Abort (MPI_COMM_WORLD, errorcode);
+  }
+    
+  // ID (rank) do processo
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // MPI_Status & MPI_Request
+  MPI_Status status;
+  MPI_Request request;
+
+  if (world_rank == 0) {
+    double x = 3.1416;
+    MPI_Isend (&x, 1, MPI_DOUBLE, 1,
+	       0, MPI_COMM_WORLD, &request);
+  } else {
+    double y = 0.0;
+    MPI_Irecv (&y, 1, MPI_DOUBLE, 0,
+	      0, MPI_COMM_WORLD, &request);
+    double x = y + 1.0;
+    printf ("x = %f\n", x);
+    int recvd = 0;
+    while (!recvd)
+      MPI_Test (&request, &recvd, &status);
+    x = y + 1;
+    printf ("x = %f\n", x);
+  }
+    
+  // Finaliza o MPI
+  MPI_Finalize ();
+
+  return 0;
+}
---
+++ src/MatematicaNumericaParalela/cap_mpi/dados/cc_sendRecvArray/sendRecvArray.cc
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// API MPI
+#include <mpi.h>
+
+int main (int argc, char** argv) {
+  // Inicializa o MPI
+  MPI_Init(NULL, NULL);
+
+  // número total de processos
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // ID (rank) do processo
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  if (world_rank == 0) {
+
+    int v[5];
+    for (int i=0; i<5; i++)
+      v[i] = i;
+    
+    MPI_Send (&v[1], 3, MPI_INT, 1,
+	      0, MPI_COMM_WORLD);
+  } else {
+    int w[5];
+    int i=0;
+    for (int j=5; j --> 0; i++)
+      w[j] = i;
+    
+    MPI_Recv (&w[0], 3, MPI_INT, 0,
+	      0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf ("Processo 1: w=\n");
+    for (int i=0; i<5; i++)
+      printf ("%d ", w[i]);
+    printf("\n");
+  }
+
+  // Finaliza o MPI
+  MPI_Finalize ();
+
+  return 0;
+}
