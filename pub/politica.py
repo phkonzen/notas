d7b1dc178ccb9763f7f828677fe87279882be258
Pedro K <phkonzen@gmail.com>
2021-05-05 19:37:34 -0300

    *: correção do navbar

--- pub/politica.py
+++ pub/politica.py
@@ -25,7 +25,6 @@ class Politica:
         head += '<title>Notas de aula</title>\n'
         head += '<meta name="author" content="Pedro H A Konzen"/>\n'
         head += '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n'
-        head += '<link rel="stylesheet" href="index.css" type="text/css">\n'
 
         # # BootstrapCDN v3.3
         # head += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">\n'
@@ -52,7 +51,11 @@ class Politica:
         head += 'gtag("js", new Date());\n'
         head += '\ngtag("config", "UA-17226092-2")\n';
         head += '</script>\n'
-        
+
+        head += '<!-- Computer Modern Serif-->'
+        head += '<link rel="stylesheet" href="fonts/cmun-serif.css"></link>'
+        head += '<link rel="stylesheet" href="index.css" type="text/css">\n'
+
         head += '</head>\n'
 
         #add at bottom
@@ -71,7 +74,7 @@ class Politica:
         # Navbar
         body += '\n\n<!-- begin: navbar -->\n'
         body += '<nav class="navbar navbar-expand-md navbar-light bg-light mb-1">\n'
-        body += '<a class="navbar-brand" href="main.html">Notas de Aula<br/><small>Início</small></a>\n'
+        body += '<a class="navbar-brand" href="main.html">Notas de Aula<br/><small>Política de dados</small></a>\n'
         body += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">\n'
         body += '<span class="navbar-toggler-icon"></span>\n'
         body += '</button>\n'
@@ -125,11 +128,11 @@ class Politica:
 
 
         body += '\n\n'
-        body += '<!-- JavaScript -->\n'
-        body += '<!-- jQuery first, then Popper.js, then Bootstrap JS -->\n'
-        body += '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>\n'
-        body += '<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>\n'
-        body += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>\n'
+        # body += '<!-- JavaScript -->\n'
+        # body += '<!-- jQuery first, then Popper.js, then Bootstrap JS -->\n'
+        # body += '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>\n'
+        # body += '<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>\n'
+        # body += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>\n'
 
         
         body += '</body>\n'
