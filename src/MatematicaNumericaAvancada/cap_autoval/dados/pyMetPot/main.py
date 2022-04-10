5cceee80d76885813066e48089660c570d881a76
Pedro Konzen <phkonzen@gmail.com>
2022-04-10 07:16:39 -0300

    +: material sobre o Método da Potência Inverso

--- src/MatematicaNumericaAvancada/cap_autoval/dados/pyMetPot/main.py
+++ src/MatematicaNumericaAvancada/cap_autoval/dados/pyMetPot/main.py
@@ -1,21 +1,20 @@
 import numpy as np
 import numpy.linalg as npla
 
-def metPot(A,q0,tol=1e-14):
-  n = A.shape[0]
+def metPot(A,q0,maxiter=1000,tol=1e-14):
   q = q0/npla.norm(q0)
-  nu0 = np.conjugate(q).dot(A @ q)
+  nu0 = np.vdot(q, np.ravel(A @ q))
   print(f"{0}: {nu0}")
 
   for k in range(maxiter):
-    z = A @ q
+    z = np.ravel(A @ q)
     q = z/npla.norm(z)
-    nu = np.conjugate(q).dot(A @ q)
+    nu = np.vdot(q, np.ravel(A @ q))
 
     print(f"{k+1}: {nu}")
     if (np.fabs(nu-nu0) < tol):
       return nu, q, 0
     elif (k+1 == maxiter):
       return nu, q, 1
-      
+
     nu0 = nu
