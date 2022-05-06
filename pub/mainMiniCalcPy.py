77822a92d7eb010fb16fa1371a518ca9f306071c
Pedro Konzen <phkonzen@gmail.com>
2022-05-06 12:19:54 -0300

    *: não há mais geração de html, apenas atualiza os .ipynb

--- pub/mainMiniCalcPy.py
+++ pub/mainMiniCalcPy.py
@@ -12,31 +12,5 @@ import os
 from sitemap import *
 from minicalcpy import *
 
-# pastas temporárias
-odir = '.docs'
-os.system('rm -rvf '+odir)
-os.system('cp -rvf ../docs '+odir)
-
-srcdir = '.src'
-os.system('mkdir -p '+srcdir)
-os.system('rm -rvf '+srcdir+'/*')
-
-# copia src para pasta temporária
-os.system('cp -rvf ../src/* '+srcdir+'/')
-
-# del as notas antigas
-os.system('rm -rvf '+odir+'/MiniCalcPy')
-
-# constroi as notas
-mini = MiniCalcPy(srcdir,odir)
-mini.build()
-
-#fonts
-os.system('cp -rvf fonts '+odir+'/MiniCalcPy/')
-
-# make sitemap.txt
-sm = SiteMap(odir)
-sm.build()
-
 # publica a atualização
-os.system('rsync -av '+odir+'/MiniCalcPy/* ../docs/MiniCalcPy/')
+os.system('rsync -av ../src/MiniCalcPy/*.ipynb ../docs/MiniCalcPy/')
