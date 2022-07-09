875feb50ac36981cff7eeafc7a370f062cc6a047
Pedro Konzen <phkonzen@gmail.com>
2022-07-09 07:49:10 -0300

    *: revisão

--- src/CalculoI/cap_deriv/dados/fig_cap_deriv_ex_rt_x2/main.py
+++ src/CalculoI/cap_deriv/dados/fig_cap_deriv_ex_rt_x2/main.py
@@ -11,12 +11,12 @@ var('x,y', real=True)
 
 x0 = 1
 
-p = plot(x**2, (x,-1,3), line_color="blue", show=False)
-q = plot(-1,(x,-1,3),line_color="none",show=False)
+p = plot(x**2, (x,-1,3.5), ylim=[-2,6], line_color="blue", show=False)
+q = plot(-1,(x,-1,3.5), ylim=[-2,6], line_color="none",show=False)
 p.extend(q)
-q = plot(2*x-1,(x,-1,3),line_color="red",show=False)
+q = plot(2*x-1,(x,-1,3.5), ylim=[-2,6], line_color="red",show=False)
 p.extend(q)
-q = plot(3*x-2,(x,-1,3),line_color="green",show=False)
+q = plot(3*x-2,(x,-1,3.5), ylim=[-2,6], line_color="green",show=False)
 p.extend(q)
 p.xlabel = '$x$'
 p.ylabel = '$y$'
@@ -26,9 +26,9 @@ fig = p._backend.fig
 ax = fig.axes[0]
 ax.grid()
 ax.set_ylim((-2,6))
-ax.plot([x0],[x0**2],marker="o",markersize=4,color="red")
-ax.plot([2],[4],marker="o",markersize=4,color="green")
-ax.text(1.75,5.1,"$y=x^2$")
-ax.text(2.52,5.4,"$y=3x-2$")
-ax.text(2.52,3.75,"$y=2x-1$")
+ax.plot([x0],[x0**2],marker="o",markersize=6,color="red")
+ax.plot([2],[4],marker="o",markersize=6,color="blue")
+ax.text(1.7,5.1,"$y=x^2$")
+ax.text(2.6,5.4,"$y=3x-2$")
+ax.text(2.6,3.75,"$y=2x-1$")
 fig.savefig('fig.png', bbox_inches='tight')
