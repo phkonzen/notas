c812af7fcc25e93b80f4d8c092500297c6ebc765
Pedro Konzen <phkonzen@gmail.com>
2022-08-31 17:28:34 -0300

    *: atualização dos anúncios

--- pub/index.py
+++ pub/index.py
@@ -147,27 +147,30 @@ class Index:
         body += '<!-- The slideshow -->'
         body += '<div class="carousel-inner">\n'
         
+        body += '<div class="carousel-item active">\n'
+        body += '<div class="spinner-grow spinner-grow-sm text-warning mb-1" role="status"></div>\n'
+        body += '<a href="https://www.ufrgs.br/ppgmap/?page_id=236">PPGMAp - UFRGS: Seleção de Mestrado 2022/2 !</a>\n'
+        body += '</div>\n\n'
+
         body += '<div class="carousel-item">\n'
         body += '<div class="spinner-grow spinner-grow-sm text-primary mb-1" role="status"></div>\n'
         body += '<a href="https://www.pec3.org/">PeC3: Conferência, Curso de Inverno, Cursos Virtuais!</a>\n'
         body += '</div>\n\n'
 
         body += '<div class="carousel-item">\n'
-        body += '<div class="spinner-grow spinner-grow-sm text-primary mb-1" role="status"></div>\n'
-        body += '<a href="http://www.cnmac.org.br">CNMAC 2022 - Inscrições Abertas !</a>\n'
-        body += '</div>\n\n'
-
-
-        body += '<div class="carousel-item active">\n'
         body += '<div class="spinner-grow spinner-grow-sm text-danger mb-1" role="status"></div>\n'
         body += '<a href="http://enmc.ccam.uesc.br/">XXV ENMC: Chamada de Trabalhos !</a>\n'
         body += '</div>\n\n'
 
         body += '<div class="carousel-item">\n'
-        body += '<div class="spinner-grow spinner-grow-sm text-warning mb-1" role="status"></div>\n'
-        body += '<a href="https://www.ufrgs.br/ppgmap/?page_id=236">PPGMAp - UFRGS: Seleção de Mestrado 2022/2 !</a>\n'
+        body += '<div class="spinner-grow spinner-grow-sm text-danger mb-1" role="status"></div>\n'
+        body += '<a href="https://eventos.fgv.br/laciam-2023">LACIAM 2023: Chamada de Trabalhos !</a>\n'
         body += '</div>\n\n'
 
+        body += '<div class="carousel-item">\n'
+        body += '<div class="spinner-grow spinner-grow-sm text-primary mb-1" role="status"></div>\n'
+        body += '<a href="http://www.cnmac.org.br">CNMAC 2022 - Inscrições Abertas !</a>\n'
+        body += '</div>\n\n'
         
         body += '<div class="carousel-item">\n'
         body += '<div class="spinner-grow spinner-grow-sm text-primary mb-1" role="status"></div>\n'
