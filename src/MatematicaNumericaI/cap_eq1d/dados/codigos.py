9bca40cc94f09e41c5d035d0d639490f5a94a581
Pedro Konzen <phkonzen@gmail.com>
2023-05-18 10:01:30 -0300

    Matemática Numérica I: revisão e novo exercício sobre o Método da Bisseção

--- src/MatematicaNumericaI/cap_eq1d/dados/codigos.py
+++ src/MatematicaNumericaI/cap_eq1d/dados/codigos.py
@@ -4,11 +4,12 @@ f = lambda x: np.sin(x+np.pi/4)**2 \
     - x**3 + np.pi/4*x**2 + 5*np.pi**2/16*x \
     + 3*np.pi**3/64
 
-a = 2
-b = 3
+a = 2.
+b = 3.
+xs = 3.*np.pi/4
 x = (a + b)/2
-print(f"0: a={a:.4f}, b={b:.4f}, x={x:.4f}")
-for k in range(10):
+print(f"\n$1$ & ${x0:.4f}$ & ${np.fabs(x - xs):.1e}$")
+for k in range(9):
     s = np.sign(f(a)*f(x))
     if (s == -1):
         b = x
@@ -17,6 +18,7 @@ for k in range(10):
     else:
         break
     x = (a + b)/2
-    print(f"{k+1}: a={a:.4f}, b={b:.4f}, x={x:.4f}")
+    print(f"${k+2}$ & ${x:.4f}$ & ${np.fabs(x-xs):.1e}$")
+    
     
 
