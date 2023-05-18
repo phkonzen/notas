b3c6a7299255c7f9fb04a56c4918c057d2d2fc01
Pedro Konzen <phkonzen@gmail.com>
2023-05-18 15:09:36 -0300

    Matemática Numérica I: revisão do material sobre o Método da Falsa Posição

--- src/MatematicaNumericaI/cap_eq1d/dados/codigos.py
+++ src/MatematicaNumericaI/cap_eq1d/dados/codigos.py
@@ -1,24 +1,32 @@
 import numpy as np
 
-f = lambda x: np.sin(x+np.pi/4)**2 \
-    - x**3 + np.pi/4*x**2 + 5*np.pi**2/16*x \
-    + 3*np.pi**3/64
+# f = lambda x: np.sin(x+np.pi/4)**2 \
+#     - x**3 + np.pi/4*x**2 + 5*np.pi**2/16*x \
+#     + 3*np.pi**3/64
 
-a = 2.
-b = 3.
-xs = 3.*np.pi/4
-x = (a + b)/2
-print(f"\n$1$ & ${x0:.4f}$ & ${np.fabs(x - xs):.1e}$")
-for k in range(9):
-    s = np.sign(f(a)*f(x))
+f = lambda x: (-x**2+1.154*x-0.332929)*np.cos(x) + x**2 - 1.154*x + 0.332929
+
+a = 0.55
+b = 0.65
+x0 = a - (b-a)/(f(b)-f(a))*f(a)
+print(f"{k+1}: {x0:.4f}")
+for k in range(100):
+    
+    s = np.sign(f(a)*f(x0))
     if (s == -1):
-        b = x
+        b = x0
     elif (s == 1):
-        a = x
+        a = x0
     else:
         break
-    x = (a + b)/2
-    print(f"${k+2}$ & ${x:.4f}$ & ${np.fabs(x-xs):.1e}$")
+
+    x = a - (b-a)/(f(b)-f(a))*f(a)
     
+    print(f"{k+2}: {x:.6f}")
+    
+    if np.fabs(x - x0) < 1e-4:
+        break
+    x0 = x
+
     
 
---
+++ src/MatematicaNumericaI/cap_eq1d/dados/fig_falsapos/main.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+     "text.usetex": True,
+     "font.family": "serif",
+     "font.size": 14
+     })
+
+f = lambda x: np.cos(x)
+
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot()
+ax.grid()
+ax.set_xlim((-np.pi/6, np.pi))
+xx = np.linspace(-np.pi/6, np.pi)
+ax.plot([-np.pi/6, np.pi], [0, 0], color='black')
+
+ax.plot(xx, f(xx))
+
+a = 0.25
+ax.plot(a, f(a), marker='o', color='blue', label='função')
+
+b = 2.
+ax.plot(b, f(b), marker='o', color='blue')
+
+rs = lambda x: (f(b)-f(a))/(b-a)*(x-a) + f(a)
+
+xx = np.linspace(0., 2.25)
+ax.plot(xx, rs(xx), color='green', label='reta secante')
+
+xa = a - (b-a)/(f(b)-f(a))*f(a)
+ax.plot(xa, rs(xa), marker='o', color="green")
+
+xs = np.pi/2
+ax.plot(xs, f(xs), marker='o', color="red")
+
+ax.set_xticks([a, xs, b],
+              ['$a$', '$x^*$', 'b'])
+ax.set_yticks([f(a), 0, f(b)],
+              ['$f(a)$', '$0$', '$f(b)$'])
+
+ax.legend()
+fig.savefig('fig.pdf')
+fig.savefig('fig.png')
