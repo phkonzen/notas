f410657868b9fd02cb9476e0d7c7b51e78f2bd53
Pedro Konzen <phkonzen@gmail.com>
2023-06-05 21:50:00 -0300

    RNA: pequenos ajustes

--- src/RNA/cap_perceptron/dados/py_e/main.py
+++ src/RNA/cap_perceptron/dados/py_e/main.py
@@ -19,29 +19,14 @@ def neuron(x, w=w, b=b):
         y[s] = activation(z)
     return y
 
-# (pré-)processamento
-def pproc_transform(x):
-    return torch.where(x == False, -1., 1.)
-
-def pproc_inverse_transform(x):
-    return torch.where(x == -1., False, True)
-
 # dados de entrada
-X = torch.tensor([[True, True],
-                  [True, False],
-                  [False, True],
-                  [False, False]])
+X = torch.tensor([[1., 1.],
+                  [1., -1.],
+                  [-1., 1.],
+                  [-1., -1.]])
 
 print(f"\nDados de entrada\n{X}")
 
-# processamento dos dados de entrada
-X_in = pproc_transform(X)
-print(f"Pré-processamento\n{X_in}")
-
 # aplicação do modelo
-y_out = neuron(X_in)
-print(f"Valores estimados\n{y_out}")
-
-# processamento dos dados de saída
-y = pproc_inverse_transform(y_out)
-print(f"(Pré-)processamento\n{y}")
+y = neuron(X)
+print(f"Valores estimados\n{y}")
