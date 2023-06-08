0d611ebc7766b9d4f194ba457e64b21a57bbfbb2
Pedro Konzen <phkonzen@gmail.com>
2023-06-08 11:19:55 -0300

    Matemática Numérica I: revisão parcial sobre o Método da Secante

--- src/MatematicaNumericaI/cap_eq1d/dados/codigos.py
+++ src/MatematicaNumericaI/cap_eq1d/dados/codigos.py
@@ -6,15 +6,19 @@ f = lambda x: np.sin(x+np.pi/4)**2 \
     - x**3 + np.pi/4*x**2 + 5*np.pi**2/16*x \
     + 3*np.pi**3/64
 
+x0 = 2.6
+x1 = 2.5
+print(f'\n0: {x0:.4f}, {x1:.4f}')
+for k in range(5):
+    x = x1 - f(x1)*(x1-x0)/(f(x1)-f(x0))
+    x0 = x1
+    x1 = x
+    print(f'{k+1}: {x0:.4f}, {x1:.4f}, {np.fabs(x1-x0):.1e}')
+
+
 fl = lambda x: -3*x**2 + np.pi*x/2 + \
     2*np.sin(x + np.pi/4)*np.cos(x + np.pi/4) + 5*np.pi**2/16
 
 fll = lambda x: -6*x - 2*np.sin(x + np.pi/4)**2 \
     + 2*np.cos(x + np.pi/4)**2 + np.pi/2
 
-x0 = -0.5
-print(f'\n0: {x0:.4f}')
-for k in range(5):
-    x = x0 - fl(x0)/fll(x0)
-    print(f'{k+1}: {x:.4f}, {np.fabs(x-x0):.1e}')
-    x0 = x
---
+++ src/MatematicaNumericaI/cap_eq1d/dados/fig_secante_geointerp/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+     "text.usetex": True,
+     "font.family": "serif",
+     "font.size": 12
+     })
+plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
+
+
+f = lambda x: (x - 2.)**2 - 1.
+fl = lambda x: 2*(x - 2.)
+
+def rt(x, x0, x1):
+    return (f(x1)-f(x0))/(x1-x0)*(x-x1) + f(x1)
+
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot()
+ax.grid()
+
+a = -0.2
+b = 1.5
+xx = np.linspace(a, b)
+ax.plot(xx, f(xx), label='função obj.')
+
+xs = 1.
+ax.plot([xs], [f(xs)], marker='o', color='red')
+
+x0 = 0
+x1 = 0.5
+ax.plot([x0], [f(x0)], marker='o', color='green')
+ax.plot([x1], [f(x1)], marker='o', color='green')
+xx = np.linspace(-0.2, 1.0)
+ax.plot(xx, rt(xx, x0, x1),
+        lw='0.75', ls='--', color='red', label='reta secante')
+
+x2 = x1 - f(x1)*(x1-x0)/(f(x1)-f(x0))
+# ax.plot([x1], [f(x1)], marker='o', color='green')
+ax.plot([x2], [f(x2)], marker='o', color='green')
+xx = np.linspace(0.25, 1.2)
+ax.plot(xx, rt(xx, x1, x2), lw='0.75', ls='--', color='red')
+
+ax.set_xticks([a, x0, x1, x2, xs, b],
+              ['', '$x^{(0)}$', '$x^{(1)}$',
+               '$x^{(2)}$', '$x^*$', ''])
+ax.set_yticks([f(a), f(x0), f(x1), f(x2), f(xs), f(b)],
+              ['', '$f(x^{(0)})$', '$f(x^{(1)})$',
+               '$f(x^{(2)})$', '$0$', ''])
+ax.legend()
+fig.savefig('fig.pdf')
+fig.savefig('fig.png')
