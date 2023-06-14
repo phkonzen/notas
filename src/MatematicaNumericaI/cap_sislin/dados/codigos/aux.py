a1813aac85bc99711e7d4c29d3275b107ff50c2c
Pedro Konzen <phkonzen@gmail.com>
2023-06-14 18:09:40 -0300

    MNI: revisão do material sobre decomposição LU

--- src/MatematicaNumericaI/cap_sislin/dados/codigos/aux.py
+++ src/MatematicaNumericaI/cap_sislin/dados/codigos/aux.py
@@ -1,4 +1,12 @@
 import numpy as np
+x = np.array([1.,2,3])
+n = x.size
+V = np.ones((n,n))
+for i in range(n-1):
+    V[:,i] = x**(n-1-i)
+print(V)
+
+import numpy as np
 import numpy.linalg as npla
 
 # mat coefs
