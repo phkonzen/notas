0be40e17745b7d5878a1d9564ed72d1658483b7f
Pedro Konzen <phkonzen@gmail.com>
2023-06-18 21:17:17 -0300

    API: material sobre instruções de ramificação

--- src/AlgoritmosProgramacaoI/cap_progest/dados/aux.py
+++ src/AlgoritmosProgramacaoI/cap_progest/dados/aux.py
@@ -1,203 +1,182 @@
-#início
+# entrada de dados
+x = float(input('Digite o valor de x:\n'))
+op = input('Digite uma das operações +, -, * ou /:\n')
+y = float(input('Digite o valor de y:\n'))
 
-# repetição: for
-for n in {-3, -2, -1, 0, 1, 2, 3}:
-    res = (n%2 == 0)
-    print(f'{n} é par? ', res)
-    
-#término
+# calcula
+if (op == '+'):
+    print(f'{x} ' + op + f' {y} = {x+y}')
+elif (op == '-'):
+    print(f'{x} ' + op + f' {y} = {x-y}')
+elif (op == '*'):
+    print(f'{x} ' + op + f' {y} = {x*y}')
+elif (op == '/'):
+    print(f'{x} ' + op + f' {y} = {x/y}')
+else:
+    print('Desculpa, não entendi!')
 
-# bloco: entrada de dados
-base = float(input('Digite a base:\n'))
-altura = float(input('Digite a altura\n'))
-
-# bloco: computação da área
-area = base*altura/2
-
-# bloco: saída de dados
-print(f'Área = {area}')
-
-#fim
-
-v = [-1, 0, 2]
-w = [3, 1, 2]
-# a)
-vpw = [v[0] + w[0],
-       v[1] + w[1],
-       v[2] + w[2]]
-print(f'a) v+w = {vpw}')
-# b)
-vmw = [v[0] - w[0],
-       v[1] - w[1],
-       v[2] - w[2]]
-print(f'b) v-w = {vmw}')
-# c)
-vdw = v[0]*w[0] + \
-      v[1]*w[1] + \
-      v[2]*w[2]
-print(f'c) v.w = {vdw}')
-# d)
-norm_v = (v[0]**2 + \
-         v[1]**2 + \
-         v[2]**2)**0.5
-print(f'd) ||v|| = {norm_v:.2f}')
-# e)
-norm_vmw = (vmw[0]**2 + \
-         vmw[1]**2 + \
-         vmw[2]**2)**0.5
-print(f'e) ||v-w|| = {norm_vmw:.2f}')
-
-
-
-
-
-a = [0,1]
-a.append(a[0]+a[1])
-a.append(a[1]+a[2])
-a.append(a[2]+a[3])
-a.append(a[3]+a[4])
-print(a)
-
-
-
-X = {-2,1,3}
-Y = {5,-1,2}
-XxY = {(-2,5), (-2,-1), (-2,2), \
-       (1,5), (1,-1), (1,2), \
-       (3,5), (3,-1), (3,2)}
-print(f'#(X x Y) = {len(XxY)}')
-
-A = {-3,-1,0,1,6,7}
-B = {-4,1,3,5,6,7}
-C = {-5,-3,1,2,3,5}
-# a)
-a = A & B
-print(f"a)\n A&B = {a}")
-# b)
-b = C | B
-print(f"b)\n A|B = {b}")
-# c)
-c = C - A
-print(f"c)\n C-A = {c}")
-# d)
-d = B & (A | C)
-print(f"d)\n B&(A|C) = {d}")
-
-
-# vértices do triangulo
-tria = {'A': (0,0), 'B': (1,0), 'C': (0,1)}
-# aresta AB
-tria['AB'] = ((tria['B'][0] - tria['A'][0])**2 \
-    + (tria['B'][1] - tria['A'][1])**2)**0.5
-# aresta BC
-tria['BC'] = ((tria['C'][0] - tria['B'][0])**2 \
-    + (tria['C'][1] - tria['B'][1])**2)**0.5
-# aresta AC
-tria['AC'] = ((tria['C'][0] - tria['A'][0])**2 \
-    + (tria['C'][1] - tria['A'][1])**2)**0.5
-# novo dicionário
-print(tria)
-
-
-A = [[-1,1,2],
-     [1,3,-1],
-     [2,0,-1]]
-
-v = [-1, 2, 1]
-w = [3, -1, 4]
-p = v[0]*w[0] \
-    + v[1]*w[1] \
-    + v[2]*w[2]
-print(f'v.w = {p}')
-
-
-s = input('Digite uma palavra:\n\t')
-print(f'A palavra {s} tem {len(s)} letras.')
 
 
 # entrada de dados
-base = float(input('Entre com o valor da base:\n\t'))
-altura = float(input('Entre com o valor da altura:\n\t'))
+print('c1: (x-a1)**2 + (y-b1)**2 = r1')
+a1 = float(input('Digite o valor de a1:\n'))
+b1 = float(input('Digite o valor de b1:\n'))
+r1 = float(input('Digite o valor de r1:\n'))
+print('c2: (x-a2)**2 + (y-b2)**2 = r1')
+a2 = float(input('Digite o valor de a2:\n'))
+b2 = float(input('Digite o valor de b2:\n'))
+r2 = float(input('Digite o valor de r2:\n'))
 
-# cálculo da área
-area = base*altura/2
+# verificações
+if (((a1==a2) and (b1==b2)) and (r11==r2)):
+    print('c1 = c2')
+else:
+    # distância entre os centros
+    dist = ((a2-a1)**2 + (b2-b1)**2)**0.5
+    if (abs(dist - (r1+r2)) < 1e-15):
+        print('c1 & c2 têm um único ponto de interseção.')
+    elif (dist < r1+r2):
+        print('c1 & c2 têm dois pontos de interseção.')
+    else:
+        print('c1 & c2 não tem ponto de interseção.')
 
-# imprime a área
-print(f'Área do triangulo de ')
-print(f'\t base = {base:e}')
-print(f'\t altura = {altura:e}')
-print(f'é igual a {area:e}')
+# entrada de dados
+print('r1: a1*x + b1 = 0')
+a1 = float(input('Digite o valor de a1:\n'))
+b1 = float(input('Digite o valor de b1:\n'))
+print('r2: a2*x + b2 = 0')
+a2 = float(input('Digite o valor de a2:\n'))
+b2 = float(input('Digite o valor de b2:\n'))
 
-x = 3
-print('É par?')
-print(x % 2 == 0)
-
-# ponto
-x = 2
-y = 0.5
-
-# y >= 0 e y <= f(x) ?
-resp1 = y >= 0 and y <= (x-1)**3
-# y >= f(x) e y <= 0 ?
-resp2 = y >= (x-1)**3 and y <= 0
-
-# conclusão
-print("O ponto está entre as curvas?")
-print(resp1 or resp2)
-
-
-# circunferência c1
-a1 = 0
-b1 = 0
-r1 = 1
-
-# circunferência c2
-a2 = 1
-b2 = 1
-r2 = 1
-
-# ponto obj
-x = 0.5
-y = 0.5
-
-# está em c1?
-em_c1 = (x-a1)**2 + (y-b1)**2 <= r1**2
-
-# está em c2?
-em_c2 = (x-a2)**2 + (y-b2)**2 <= r2**2
-
-# está em c1 e c2?
-resp = em_c1 and em_c2
-print('O ponto está na interseção de c1 e c2?', resp)
-
-
-# ponto
-x = 1
-y = 1
-
-# centro circunferência
-a = 0
-b = 0
-# raio circunferência
-raio = 1
-
-# verifica se está no disco
-v = (x-a)**2 + (y-b)**2 <= raio**2
-
-# imprime resposta
-print('O ponto está no disco?', v)
+# resultado
+if (a1 == a2):
+    if (b1 == b2):
+        print('r1 = r2')
+    else:
+        print('r1 // r2 e r1 != r2')
+else:
+    x = (b1-b2)/(a2-a1)
+    y = a1*x + b1
+    print('Ponto de interseção: ({x}, {y}).')
 
 
 
-# parametros
-a1 = 1
-a2 = -1
-b1 = 1
-b2 = -1
-# ponto x de interseção
-x_intercep = (b2-b1)/(a1-a2)
-# ponto y de interceção
-y_intercep = a1*x_intercep + b1
-# imprime o resultado
-print(f'x_i = {x_intercep:e}')
-print(f'y_i = {y_intercep:e}')
+
+
+# entrada de dados
+print('Circunferência c:')
+a = float(input('Digite o valor de a:\n'))
+b = float(input('Digite o valor de b:\n'))
+r = float(input('Digite o valor de r:\n'))
+print('Ponto (x, y):')
+x = float(input('Digite o valor de x:\n'))
+y = float(input('Digite o valor de y:\n'))
+
+# resultado
+if ((x-a)**2 + (y-b)**2 <= r):
+    print(f'({x}, {y}) pertence ao disco.')
+else:
+    print(f'({x}, {y}) não pertence ao disco.')
+
+# computação
+if (a != 0):
+    x = -b/a
+    y = a*x + b
+    print(f'Intercepta eixo-x em: ({x}, {y}).')
+
+# entrada de dados
+print('c1: (x-a1)**2 + (y-b1)**2 = r1')
+a1 = float(input('Digite o valor de a1:\n'))
+b1 = float(input('Digite o valor de b1:\n'))
+r1 = float(input('Digite o valor de r1:\n'))
+print('c2: (x-a2)**2 + (y-b2)**2 = r1')
+a2 = float(input('Digite o valor de a2:\n'))
+b2 = float(input('Digite o valor de b2:\n'))
+r2 = float(input('Digite o valor de r2:\n'))
+print('Ponto de interesse (x,y).')
+x = float(input('Digite o valor de x:\n'))
+y = float(input('Digite o valor de y:\n'))
+
+# pertence ao disco c1?
+c1 = (x-a1)**2 + (y-b1)**2 <= r1
+# pertence ao disco c2?
+c2 = (x-a2)**2 + (y-b2)**2 <= r2
+
+# imprime resultado
+if (c1 and c2):
+    print(f'({x}, {y}) pertence à interseção dos discos.')
+elif (c1):
+    print(f'({x},{y}) pertence ao disco c1.')
+elif (c2):
+    print(f'({x},{y}) pertence ao disco c2.')
+else:
+    print(f'({x},{y}) não pertence aos discos.')
+
+
+
+# entrada de dados
+print('Coeficientes da parábola')
+print('a1*x**2 + a2*x + a3 = 0')
+a1 = float(input('Digite o valor de a1:\n'))
+a2 = float(input('Digite o valor de a2:\n'))
+a2 = float(input('Digite o valor de a3:\n'))
+
+print('Coeficientes da reta')
+b1 = float(input('Digite o valor de b1:\n'))
+b2 = float(input('Digite o valor de b2:\n'))
+
+# discriminante da equação
+# a1*x**2 + (a2-b1)*x + (a3-b2) = 0
+delta = (a2-b1)**2 - 4*a1*(a3-b2)
+
+# ponto(s) de interseção
+if (delta == 0):
+    x = (b1-a2)/(2*a1)
+    y = b1*x + b2
+    print('Ponto de interseção:')
+    print(f'({x}, {y})')
+elif (delta > 0):
+    x1 = ((b1-a2) - delta**2)/(2*a1)
+    y1 = b1*x1 + b2
+    x2 = ((b1-a2) + delta**2)/(2*a1)
+    y2 = b1*x2 + b2
+    print('Pontos de interseção:')
+    print(f'({x1}, {y1}), ({x2}, {y2})')
+else:
+    print('Não há ponto de interseção.')
+    
+
+# discriminante
+delta = b**2 - 4*a*c
+
+# raízes
+if (delta > 0):
+    # raízes distintas
+    x1 = (-b - delta**0.5)/(2*a)
+    x2 = (-b + delta**0.5)/(2*a)
+    print('Raízes reais distintas:')
+    print(f'x_1 = {x1}')
+    print(f'x_2 = {x2}')
+elif (delta == 0):
+    # raiz dupla
+    x = -b/(2*a)
+    print('Raiz dupla:')
+    print('x_1 = x_2 = {x}')
+else:
+    # raízes complexas
+    # parte real
+    rea = -b/(2*a)
+    # parte imaginária
+    img = (-delta)**0.5/(2*a)
+    x1 = rea - img*1j
+    x2 = rea + img*1j
+    print('Raízes complexas:')
+    print(f'x_1 = {x1}')
+    print(f'x_2 = {x2}')
+
+
+x = 0
+if (x >= 0):
+    y = 1
+x += y
+print(x)
