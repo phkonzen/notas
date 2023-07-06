7141695e4b25d1ae5e7eafcabb2d2bfc608c87c7
Pedro Konzen <phkonzen@gmail.com>
2023-07-06 10:11:34 -0300

    MNII: revisão do material sobre o Método de Taylor

--- src/MatematicaNumericaII/cap_pvi/dados/codigos/aux.py
+++ src/MatematicaNumericaII/cap_pvi/dados/codigos/aux.py
@@ -1,23 +1,44 @@
-import math as m
+import sympy as sym
+y = sym.Function('y')
+t = sym.Symbol('t')
+eq = sym.Eq(y(t).diff(t), y(t)**2 - t*y(t))
+sol = sym.dsolve(eq)
+# sol = sym.dsolve(eq, ics={y(1): 0})
+print(sol)
+
 import numpy as np
 
-def euler(f, t0, y0, h, n):
+def taylor(Phi, t0, y0, h, n):
     t = t0
     y = y0
     for k in range(n):
-        y += h*f(t, y)
+        y += h*Phi(t, y, h)
         t += h
-        # print(k+1, t, y)
     return t, y
 
 def f(t, y):
-    return -30*y
+    return t**2*y
 
-def exata(t):
-    return 1./3*np.exp(-30*t)
+def fl(t, y):
+    return 2*t*y + t**2*f(t,y)
 
-h = 1e-1
-n = round(1/h)
-t,y = euler(f, 0., 1./3, h, n)
-print(f'{h}: {t}, {y:.5E}, {np.abs(y - exata(1.5)):.1E}')
-#print(f'{h}: {t}, {y:.4E}')
+def Phi(t, y, h):
+    return f(t, y) + h/2*fl(t, y) 
+
+# # analítica
+# def exata(t):
+#     return 0.5*np.cos(t) - 0.5*np.sin(t)
+
+t0 = 1.
+y0 = 0.5
+tf = 3.
+h = 1e-6
+n = round((tf-t0)/h)
+t, y = taylor(Phi, t0, y0, h, n)
+
+# import matplotlib.pyplot as plt
+# plt.plot(t, np.abs(y - exata(t)), ls='--', marker='.')
+# plt.grid()
+# plt.show()
+
+print(f'\n{h:.1E}, {y:.5E}')
