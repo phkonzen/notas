a617ac7a4d709b34c44dde960c3061a38a762167
Pedro Konzen <phkonzen@gmail.com>
2023-07-12 10:24:01 -0300

    MNII: adicionando material sobre o Método de Euler Implícito

--- src/MatematicaNumericaII/cap_pvi/dados/codigos/eulerImp.py
+++ src/MatematicaNumericaII/cap_pvi/dados/codigos/eulerImp.py
@@ -1,14 +1,6 @@
 import numpy as np
 from scipy.optimize import fsolve
 
-def euler(f, t0, y0, h, n):
-    t = t0
-    y = y0
-    for k in range(n):
-        y = y + h*f(t,y)
-        t += h
-    return t, y
-
 def eulerimp(f, t0, y0, h, n):
     t = t0
     y = y0
@@ -20,13 +12,13 @@ def eulerimp(f, t0, y0, h, n):
     return t, y
 
 def f(t, y):
-    return 50 - 50*y
+    return y + np.sin(t)
 
 # analítica
 def exata(t):
-    return 1 + np.exp(-50*t)
+    return np.exp(t) - 0.5*np.sin(t) - 0.5*np.cos(t)
 
 h = 1e-1
 n = round(1./h)
-t,y = eulerimp(f, 0., 2., h, n)
-print(f'{h:.1e}: {y:.5e} {np.abs(y-exata(1.)):.1e}')
+t,y = eulerimp(f, 0., 0.5, h, n)
+print(f'{h:.1e}: {y:.5e} {np.abs(y-exata(1)):.1e}')
