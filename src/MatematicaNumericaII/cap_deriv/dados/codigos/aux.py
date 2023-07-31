77aa5c2ac137bf81aeaa63f536ba7c8b264fe37a
Pedro Konzen <phkonzen@gmail.com>
2023-07-31 09:23:44 -0300

    ajuste parcial nas referências bibliográficas

--- src/MatematicaNumericaII/cap_deriv/dados/codigos/aux.py
+++ src/MatematicaNumericaII/cap_deriv/dados/codigos/aux.py
@@ -4,16 +4,23 @@ def dfc_h2(f, x, h=1e-7):
     df = (f(x+h) - f(x-h))/(2*h)
     return df
 
-def dfr_h(f, x, h=1e-7):
-    df = (f(x) - f(x-h))/h
+def dfr_h2(f, x, h=1e-7):
+    df = 1/(2*h)*(3*f(x) - 4*f(x-h) + f(x-2*h))
     return df
 
-def dfp_h(f, x, h=1e-7):
-    df = (f(x+h) - f(x))/h
+def dfp_h2(f, x, h=1e-7):
+    df = 1/(2*h)*(-3*f(x) + 4*f(x+h) - f(x+2*h))
     return df
 
 f = lambda x: np.cos(x)
-x = np.pi/3
-h = 1e-3
-dfdx = dfc_h2(f, x, h)
-print(f'{h}: dfdx = {dfdx:.5e}, erro = {np.fabs(dfdx-0.5):.1e}')
+x = np.linspace(2,2.5,6)
+y = np.array([1.86,
+              1.90,
+              2.01,
+              2.16,
+              2.23,
+              2.31])
+df1 = 1/0.2*(-3*y[0] + 4*y[1] - y[2])
+print(df1)
+df2 = 1/0.2*(3*y[5] - 4*y[4] + y[3])
+print(df2)
