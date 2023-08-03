5713dc92fd3d4fe99abca6916447010e759d9d28
Pedro Konzen <phkonzen@gmail.com>
2023-08-03 19:54:48 -0300

    MNII: revisão do material sobre derivadas de segunda ordem e fórumos via polinômios de Taylor

--- src/MatematicaNumericaII/cap_deriv/dados/codigos/d2fc_h2.py
+++ src/MatematicaNumericaII/cap_deriv/dados/codigos/d2fc_h2.py
@@ -4,8 +4,8 @@ def d2fc_h2(f, x, h=1e-7):
     df = (f(x+h) - 2*f(x) + f(x-h))/h**2
     return df
 
-f = lambda x: np.exp(x)*np.log(x+1)-x
-x = 1.
-h = 1e-3
+f = lambda x: x**2 + np.sin(x)
+x = np.pi/6
+h = 1e-1
 d2fdx2 = d2fc_h2(f, x, h)
-print(f'{h}: d2fdx2 = {d2fdx2:.5e}')
+print(f'{h}: d2fdx2 = {d2fdx2:.5e}, erro = {np.fabs(d2fdx2-1.5):.1e}')
