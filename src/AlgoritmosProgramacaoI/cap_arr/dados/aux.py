9b12806c469304f3cc3d4aa63ba90a0dfc283a09
Pedro Konzen <phkonzen@gmail.com>
2023-08-10 08:07:19 -0300

    API: correção sugerida via formulário do site

--- src/AlgoritmosProgramacaoI/cap_arr/dados/aux.py
+++ src/AlgoritmosProgramacaoI/cap_arr/dados/aux.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.linalg as npla
 # matriz dos coefs
 A = np.array([[2, -1, 1],
               [-1, 1, 3],
@@ -6,6 +7,29 @@ A = np.array([[2, -1, 1],
 # vet termos consts
 b = np.array([-3, 6, 2])
 
-B = np.array([[1, -1],
-              [2, 1],
-              [1, 0]])
+# det(A)
+detA = npla.det(A)
+print(f'det(A) = {detA}')
+
+# matrizes auxiliares
+## A1
+A1 = np.copy(A)
+A1[:,0] = b
+detA1 = npla.det(A1)
+print(f'det(A1) = {detA1}')
+
+## A2
+A2 = np.copy(A)
+A2[:,1] = b
+detA2 = npla.det(A2)
+print(f'det(A2) = {detA2}')
+
+## A3
+A3 = np.copy(A)
+A3[:,2] = b
+detA3 = npla.det(A3)
+print(f'det(A3) = {detA3}')
+
+# solucao
+x = np.array([detA1/detA, detA2/detA, detA3/detA])
+print(f'x = {x}')
