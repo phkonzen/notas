cd9479049cf898fbcbb8528c5a78d7fce98b06e8
Pedro Konzen <phkonzen@gmail.com>
2023-08-11 21:56:58 -0300

    MNII: correções sugeridas via formulário do site

--- src/MatematicaNumericaII/cap_integr/dados/codigos/aux.py
+++ src/MatematicaNumericaII/cap_integr/dados/codigos/aux.py
@@ -1,11 +1,16 @@
 import numpy as np
 from numpy.polynomial.legendre import leggauss
 
-# integrando
-f = lambda x: np.cos(x)
+# integral
+a = 0
+b = 1
+f = lambda x: x*np.exp(-x**2)
 # quadratura
-n = 4
+n = 2
 x,w = leggauss(n)
+# mud de var
+x = (b-a)/2*(x+1)+a
+w = (b-a)/2*w
 # aproximação
 S = np.sum(f(x)*w)
 print(f'{n}: S = {S:.5e}')
