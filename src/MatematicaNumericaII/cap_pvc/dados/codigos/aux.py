4d9efd49092f40940264c2f0fee5ea3b8a1c6458
Pedro Konzen <phkonzen@gmail.com>
2023-08-29 07:00:50 -0300

    MNII: correção sugerida via formulário do site

--- src/MatematicaNumericaII/cap_pvc/dados/codigos/aux.py
+++ src/MatematicaNumericaII/cap_pvc/dados/codigos/aux.py
@@ -1,80 +1,43 @@
 import numpy as np
 import numpy.linalg as npla
-from numpy import pi, sin, cos
 import matplotlib.pyplot as plt
 
-# parâmetros
-n = 10000
-h = 1./n
-xx = np.linspace(0., 1., n+1)
+plt.rcParams.update({
+     "text.usetex": True,
+     "font.family": "serif",
+     "font.size": 14
+     })
 
-# c.c. Dirichlet
-ua = 1.
-ub = -1.
 
-def f(x, u, ux):
-    return u**2 - cos(pi*x)**2 - pi**2*cos(pi*x)
+# malha
+n = 200
+h = 2./n
+xx = np.linspace(-1., 1., n+1)
 
-def fu(x, u, ux):
-    return 2*u
+# fonte
+def f(x):
+    return 1. if x<=0. else 0.
 
-def fux(x, u, ux):
-    return 0.
+# prob discreto
+A = np.zeros((n+1, n+1))
+b = np.empty(n+1)
 
-# rhs
-def F(u):
-    y = np.empty(n+1)
-    # f_1
-    y[0] = u[0] - ua
-    # f_i
-    for i in range(1,n):
-        ux = u[i+1]/(2*h) - u[i-1]/(2*h) 
-        y[i] = -1./h**2*u[i-1] + 2./h**2*u[i] - 1./h**2*u[i+1] \
-            + f(xx[i], u[i], ux)
-    # f_{n+1}
-    y[n] = u[n] - ub
+# c.c. x = 0.
+A[0,0] = 1.
+b[0] = 0.
 
-    return y
+# pts internos
+for i in range(1,n):
+    A[i,i-1] = -1./h**2 - 1./(2*h)
+    A[i,i] = 2./h**2
+    A[i,i+1] = -1./h**2 + 1./(2*h)
+    b[i] = f(xx[i])
 
-# jacobiana
-def J(u):
-    J = np.zeros((n+1,n+1))
-    J[0,0] = 1.
-    for i in range(1,n):
-        ux = 1./(2*h)*u[i+1] - 1./(2*h)*u[i-1]
-        J[i,i-1] = -1./h**2 - 1/(2*h)\
-            * fux(xx[i], u[i], ux)
-        J[i,i] = 2/h**2 + fu(xx[i], u[i], ux)
-        J[i,i+1] = -1./h**2 + 1/(2*h)\
-            * fux(xx[i], u[i], ux)
-    J[n,n] = 1.
+# c.c. x = 1.
+A[n,n] = 1.
+b[n] = 0.
 
-    return J
+# resol
+u = npla.solve(A, b)
 
-# aprox inicial
-u = np.zeros(n+1)
-
-# iterações de Newton
-maxiter = 10
-for k in range(maxiter):
-
-    # passo de Newton
-    dlta = npla.solve(J(u), -F(u))
-
-    # atualização
-    u += dlta
-
-    ndlta = npla.norm(dlta)
-    print(f'{k+1}: norm = {ndlta:.2e}')
-    if (ndlta < 1e-10):
-        print('convergiu.')
-        break
-
-def ue(x):
-    return cos(pi*x)
-
-print(f'{h:.1e}: err ={ npla.norm(u - ue(xx)):.1e}')
-
-#plt.plot(xx, u)
-#plt.plot(xx, ue(xx))
-#plt.show()
+print(xx[n//2], u[n//2])
