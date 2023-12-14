f81d1f21140707e1d17985487eacb1f68c58ccbe
Pedro Konzen <phkonzen@gmail.com>
2023-12-14 10:40:46 -0300

    MEF: reorganização de seções e novo material de aplicação na eq. Fisher

--- src/MetodoElementosFinitos/cap_mef1d/dados/ex_mef1d_fisher/main.py
+++ src/MetodoElementosFinitos/cap_mef1d/dados/ex_mef1d_fisher/main.py
@@ -8,14 +8,13 @@ from dolfinx.fem.petsc import NonlinearProblem
 from dolfinx.nls.petsc import NewtonSolver
 
 # parâmetros
-tf = 1.
-alpha = 1.
+tf = 5.
 
 # esquema theta
 theta = 0.5
 
 # discretização no tempo
-nt = 10
+nt = 100
 ht = tf/nt
 
 # malha
@@ -27,15 +26,16 @@ x = ufl.SpatialCoordinate(domain)
 V = fem.functionspace(domain, ('P', 1))
 
 # mef fun.s
-uh = fem.Function(V)
 v = ufl.TestFunction(V)
+u = fem.Function(V)
 
 # condição inicial
 t = 0.
 u0 = fem.Function(V)
-u0.interpolate(lambda x: 1./10*np.cos(np.pi*x[0])**2)
+u0.interpolate(lambda x: np.cos(np.pi*x[0])**2)
 
-uh.x.array[:] = u0.x.array[:]
+# inicialização
+u.x.array[:] = u0.x.array[:]
 
 # visualização (paraview)
 from dolfinx import io
@@ -47,31 +47,36 @@ results_folder.mkdir(exist_ok=True, parents=True)
 filename = results_folder / f"u_{0:0>6}"
 with io.XDMFFile(domain.comm, filename.with_suffix(".xdmf"), "w") as xdmf:
     xdmf.write_mesh(domain)
-    xdmf.write_function(uh, 0.)
+    xdmf.write_function(u, 0.)
 
 
 # iteração no tempo
 for k in range(nt):
+    
     t += ht
+    print(f"{k+1}: t = {t:.4g}")
 
     # forma fraca
-    F = theta * ufl.dot(ufl.grad(uh), ufl.grad(v)) * ufl.dx
-    F -= theta * uh * (1. - uh) * v * ufl.dx
-    F += uh * v / ht * ufl.dx
-
-    F -= (theta-1.) * ufl.dot(ufl.grad(u0), ufl.grad(v)) * ufl.dx
+    ## time term
+    F = 1./ht * u * v * ufl.dx
+    F -= 1./ht * u0 * v * ufl.dx
+    ## diffusion term
+    F += theta * ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    F += (1.-theta) * ufl.dot(ufl.grad(u0), ufl.grad(v)) * ufl.dx
+    ## reaction term
+    F -= theta * u * (1. - u) * v * ufl.dx
     F -= (1.-theta) * u0 * (1. - u0) * v * ufl.dx
-    F -= u0 * v / ht * ufl.dx
 
-    problem = NonlinearProblem(F, uh)
+    problem = NonlinearProblem(F, u)
     solver = NewtonSolver(MPI.COMM_WORLD, problem)
-    n, converged = solver.solve(uh)
+    n, converged = solver.solve(u)
+    print(f"\tNewton iterations: {n}")
     assert(converged)
 
     # armazena para visualização (paraview)
     filename = results_folder / f"u_{k+1:0>6}"
     with io.XDMFFile(domain.comm, filename.with_suffix(".xdmf"), "w") as xdmf:
         xdmf.write_mesh(domain)
-        xdmf.write_function(uh, t)
+        xdmf.write_function(u, t)
 
-    u0.x.array[:] = uh.x.array[:]
+    u0.x.array[:] = u.x.array[:]
